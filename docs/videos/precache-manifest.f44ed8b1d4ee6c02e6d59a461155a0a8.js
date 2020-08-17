0849bb4249c30c64217edc321ac7b6f67d1c3b5e
Jeremy Howard <j@fast.ai>
2020-08-17 16:51:12 -0700

    rebuild

---
+++ docs/videos/precache-manifest.f44ed8b1d4ee6c02e6d59a461155a0a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "375143fd753294415c31",
+    "url": "/static/css/main.0dd71799.chunk.css"
+  },
+  {
+    "revision": "375143fd753294415c31",
+    "url": "/static/js/main.33ae3a67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "018d8b9588400498ab27",
+    "url": "/static/css/2.c09e2c2f.chunk.css"
+  },
+  {
+    "revision": "018d8b9588400498ab27",
+    "url": "/static/js/2.408c149b.chunk.js"
+  },
+  {
+    "revision": "27d70a034230fc4e4d3d7a93e543b1fa",
+    "url": "/index.html"
+  }
+];
--- docs/videos/service-worker.js
+++ docs/videos/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c410cdceaa0c81396032f02f7432b82.js"
+  "/precache-manifest.f44ed8b1d4ee6c02e6d59a461155a0a8.js"
 );
 
 workbox.clientsClaim();
