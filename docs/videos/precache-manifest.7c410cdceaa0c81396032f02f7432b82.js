d8071a2e30a45c92f69e149524304da6cbe8fb52
Jeremy Howard <j@fast.ai>
2020-08-17 15:38:55 -0700

    rebuild

--- docs/videos/precache-manifest.7c410cdceaa0c81396032f02f7432b82.js
+++ docs/videos/precache-manifest.7c410cdceaa0c81396032f02f7432b82.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5eafce05af670f28815b",
+    "revision": "a2ed134b47e29d366011",
     "url": "/static/css/main.0dd71799.chunk.css"
   },
   {
-    "revision": "5eafce05af670f28815b",
-    "url": "/static/js/main.e89b1c6c.chunk.js"
+    "revision": "a2ed134b47e29d366011",
+    "url": "/static/js/main.9e867e5c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.1e1d7b85.chunk.js"
   },
   {
-    "revision": "068f541cf7b6f9ea65c91429aaa729cf",
+    "revision": "6b08c9167cee57e8aaa2fe56fdd90d60",
     "url": "/index.html"
   }
 ];
